3ce1efa923d6e2d8411b2ea2c12675e7d3d48140
guleff <guleffdev@mail.bg>
2014-09-09 13:54:24 +0300

    folder name changed

--- Level-1/JavaScript Basics/Homework-guleff-JavaScript Syntax/cylinderVol.js
+++
@@ -1,9 +0,0 @@
-function calcCylinderVol(r,h){
-
-var volume=Math.round(Math.PI*r*r*h*1000)/1000;
-
-return volume;
-}
-console.log(calcCylinderVol(2,4));
-console.log(calcCylinderVol(5,8));
-console.log(calcCylinderVol(12,3));
